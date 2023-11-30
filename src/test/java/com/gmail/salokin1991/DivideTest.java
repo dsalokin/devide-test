f84d03de7a1d7ba70927bf3fb98a9a744a819771
sardanaivanova <sardana.ivanova@qameta.io>
2023-11-30 15:11:33 +0300

    change parameters and add test arithmetic exception

--- src/test/java/com/gmail/salokin1991/DivideTest.java
+++ src/test/java/com/gmail/salokin1991/DivideTest.java
@@ -3,12 +3,26 @@ package com.gmail.salokin1991;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class DivideTest {
 
     @Test
     void testDivide() {
         Divide divide = new Divide();
-        Assertions.assertEquals(divide.divideAB(), 20);
+        Assertions.assertEquals(divide.divideAB(80, 4), 20);
+    }
+    @Test
+    void testArithmeticExc() {
+        Divide divide = new Divide();
+//        Assertions.assertEquals(divide.divideAB(80, 0), 20);
+        assertThatThrownBy(() -> {
+         divide.divideAB(80, 0);
+        })
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessageContaining("/ by zero");
     }
 
 }
